c5fd2d90cc2bdc24fa730ff5c36e34eda95cedf7
Rafael Elberg <rafael.elberg@uc.cl>
2021-10-03 18:57:47 -0300

    Implementando OPcodes

--- Proyecto Base/Proyecto Base.srcs/sources_1/new/RAM.vhd
+++ Proyecto Base/Proyecto Base.srcs/sources_1/new/RAM.vhd
@@ -5,7 +5,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
-entity RAM is 
+entity RAM is
     Port (
         clock       : in   std_logic;
         write       : in   std_logic;
@@ -16,10 +16,9 @@ entity RAM is
 end RAM;
 
 architecture Behavioral of RAM is
-type RAM_array is array (0 to ( 2 ** 12 )-1) of std_logic_vector (15 downto 0);
-	signal memory : RAM_array;
 
---declaring components
+    type memory_array is array (0 to ( 2 ** 12 ) - 1  ) of std_logic_vector (15 downto 0);
+	signal memory : memory_array;
 	
 begin
 
--- Proyecto Base/Proyecto Base.srcs/sources_1/new/control_unit.vhd
+++ Proyecto Base/Proyecto Base.srcs/sources_1/new/control_unit.vhd
@@ -32,21 +32,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity control_unit is
-    Port ( instruc : in STD_LOGIC_VECTOR (20 downto 0);
+    Port ( instruct : in STD_LOGIC_VECTOR (20 downto 0);
            c_stat : in STD_LOGIC;
            n_stat : in STD_LOGIC;
            z_stat : in STD_LOGIC;
            enA : out STD_LOGIC;
            enB : out STD_LOGIC;
-           selA : out STD_LOGIC_VECTOR (2 downto 0);
-           selB : out STD_LOGIC_VECTOR (2 downto 0);
+           selA : out STD_LOGIC_VECTOR (1 downto 0);
+           selB : out STD_LOGIC_VECTOR (1 downto 0);
            loadPC : out STD_LOGIC;
-           selALU : out STD_LOGIC_VECTOR (3 downto 0));
+           selALU : out STD_LOGIC_VECTOR (2 downto 0);
+           w : out STD_LOGIC);
 end control_unit;
+
 
 architecture Behavioral of control_unit is
 
-begin
+signal output: std_logic_vector(10 downto 0);
+
+
 
 
 end Behavioral;
